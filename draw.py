d52429cfc1752e3cb0b55d5e1e3763bd728d0544
jchua1 <jchua@stuy.edu>
2017-02-14 15:37:27 -0500

    octant update

--- draw.py
+++ draw.py
@@ -1,12 +1,23 @@
 from display import *
-"""
-def draw_line( screen, x0, y0, x1, y1, color ):
+
+def draw_line(screen, x0, y0, x1, y1, color):
+    slope = (y1 - y0) / (x1 - x0)
+    if 0 <= slope <= 1:
+        octant_1(screen, x0, y0, x1, y1, color)
+    elif slope > 1:
+        octant_2(screen, x0, y0, x1, y1, color)
+    elif -1 <= slope < 0:
+        octant_8(screen, x0, y0, x1, y1, color)
+    else:
+        octant_7(screen, x0, y0, x1, y1, color)
+
+def octant_1(screen, x0, y0, x1, y1, color):
     x = x0
     y = y0
     A = y1 - y0
     B = -(x1 - x0)
     d = 2 * A + B
-    while x <= x1:
+    while x < x1:
         plot(screen, color, x, y)
         if d > 0:
             y += 1
@@ -14,18 +25,48 @@ def draw_line( screen, x0, y0, x1, y1, color ):
         x += 1
         d += 2 * A
     pass
-"""
-def draw_line( screen, x0, y0, x1, y1, color ):
+
+def octant_2(screen, x0, y0, x1, y1, color):
+    x = x0
+    y = y0
+    A = y1 - y0
+    B = -(x1 - x0)
+    d = A + 2 * B
+    while y < y1:
+        plot(screen, color, x, y)
+        if d < 0:
+            x += 1
+            d += 2 * A
+        y += 1
+        d += 2 * B
+    pass
+
+def octant_8(screen, x0, y0, x1, y1, color):
     x = x0
     y = y0
     A = y1 - y0
     B = -(x1 - x0)
     d = 2 * A - B
-    while x <= x1:
+    while x < x1:
         plot(screen, color, x, y)
         if d < 0:
             y -= 1
             d -= 2 * B
         x += 1
         d += 2 * A
+    pass
+
+def octant_7(screen, x0, y0, x1, y1, color):
+    x = x0
+    y = y0
+    A = y1 - y0
+    B = -(x1 - x0)
+    d = A - 2 * B
+    while y > y1:
+        plot(screen, color, x, y)
+        if d > 0:
+            x += 1
+            d += 2 * A
+        y -= 1
+        d -= 2 * B
     pass
--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-draw_line(screen, 0, 500, 500, 250, color)
+draw_line(screen, 0, 500, 500, 0, color)
 display(screen)
 save_extension(screen, 'img.png')
